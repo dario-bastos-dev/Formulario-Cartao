e54ba9e540d0b193123ca4f2392269fb361571df
Dario Bastos <dario.bastos.dev@gmail.com>
2024-03-30 12:00:46 -0300

    Adicionando Class

--- js/script.js
+++ js/script.js
@@ -1,10 +1,47 @@
-function atualizarNome() {
-  let inputName = document.querySelector("#inputName").value;
+//Class do Cartão de crédito
+class Cartao {
 
-  document.querySelector("#nome-proprietario").innerHTML = inputName.toUpperCase()
+  // Function para adicionar o nome
+  setNomeProprietario(v) {
+    
 
-  if(inputName == "") {
-    document.querySelector("#nome-proprietario").innerHTML = "JANE APPLESEED"
+    document.querySelector("#nome-proprietario").innerHTML =
+      v.toUpperCase();
+
+    if (v == "") {
+      document.querySelector("#nome-proprietario").innerHTML = "JANE APPLESEED";
+    }
   }
 
+  //Function para adicionar o número
+  setNumero(v) {
+
+    
+  }
+}
+
+//Instanciando a class
+const cartao = new Cartao();
+
+// Adicionando o Nome do Proprietário
+let inputName = document.querySelector("#inputName");
+
+inputName.addEventListener("keyup", atualizarName);
+//-------------------
+function atualizarName() {
+
+  let inputNameValue = document.querySelector("#inputName").value;
+
+  cartao.setNomeProprietario(inputNameValue)
+}
+
+// Adicionando o Número do Cartão
+let inputNumb = document.querySelector("#inputCardNumber");
+
+inputNumb.addEventListener("keyup", atualizarNumb);
+//---------------------
+function atualizarNumb() {
+  let key = document.querySelector("#inputCardNumber").value;
+
+  let numb = cartao.setNumero(key)
 }
