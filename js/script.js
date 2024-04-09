9b6520aaf3d7a79aaeb301c7032b00e0e94ea794
Dario Bastos <dario.bastos.dev@gmail.com>
2024-04-08 21:12:10 -0300

    Adicionando a verificação

--- js/script.js
+++ js/script.js
@@ -1,8 +1,20 @@
+//Aplicando mascara JQuery
+$(document).ready(function() {
+  $("#inputCardNumber").mask("0000 0000 0000 0000")
+
+  $("input.cvc").mask("000")
+
+  $("input.data-mes").mask("00")
+  $("input.data-ano").mask("00")
+})
+
 // Adicionando o Nome do Proprietário
 let inputName = document.querySelector("#inputName");
 
 inputName.addEventListener("keyup", atualizarName);
-//-------------------
+inputName.addEventListener("keyup", verificarNome);
+
+//Função para atualizar nome
 function atualizarName() {
   let inputNameValue = document.querySelector("#inputName").value;
 
@@ -13,19 +25,32 @@ function atualizarName() {
     };
 }
 
+//Verificação do nome
+function verificarNome() {
+  let value = document.querySelector("#inputName").value;
+
+  let h6 = document.querySelector("h6.area-1")
+  let input = document.querySelector("input.input-1")
+
+  if(value == "" || value.length < 16 || value.match(/\d/)) {
+    h6.classList.remove("hide")
+    input.classList.add("input-border")
+    return false
+    
+  } else {
+    h6.classList.add("hide")
+    input.classList.remove("input-border")
+    return true
+  }
+}
+
 // Adicionando o Número do Cartão
 let inputNumb = document.querySelector("#inputCardNumber");
 
-inputNumb.addEventListener("keyup", atualizarNumb, );
+inputNumb.addEventListener("keyup", atualizarNumb);
+inputNumb.addEventListener("keyup", verificarNumero);
 
-//Aplicando mascara JQuery
-$(document).ready(function() {
-  $("#inputCardNumber").mask("0000 0000 0000 0000")
-
-  $("input.cvc").mask("000")
-})
-
-//---------------------
+//Função para atualizar o número
 function atualizarNumb() {
   let key = document.querySelector("#inputCardNumber").value;
 
@@ -37,13 +62,36 @@ function atualizarNumb() {
   
 }
 
+//Verificação do número
+function verificarNumero() {
+  let value = document.querySelector("#inputCardNumber").value;
+
+  let h6 = document.querySelector("h6.area-2")
+  let input = document.querySelector("input.input-4")
+
+  if(value == "" || value.length < 16) {
+    h6.classList.remove("hide")
+    input.classList.add("input-border")
+    return false
+    
+  } else {
+    h6.classList.add("hide")
+    input.classList.remove("input-border")
+    return true
+  }
+}
+
 //Adicionado a data e ano do Cartão
 let inputMes = document.querySelector(`div input.data-mes`)
 let inputAno = document.querySelector(`div input.data-ano`)
 
 inputMes.addEventListener(`keyup`, atualizarData)
 inputAno.addEventListener(`keyup`, atualizarData)
-//----------------------------
+
+inputMes.addEventListener(`keyup`, verificarData)
+inputAno.addEventListener(`keyup`, verificarData)
+
+//Função para atualizar a data
 function atualizarData() {
   let mes = document.querySelector(`.data-mes`).value
   let ano = document.querySelector(`.data-ano`).value
@@ -64,11 +112,41 @@ function atualizarData() {
   }
 }
 
+//Verificação da data
+function verificarData() {
+  let mes = document.querySelector(`.data-mes`).value
+  let ano = document.querySelector(`.data-ano`).value
+
+  let h6 = document.querySelector("h6.area-3")
+  let input = document.querySelector("input.input-2")
+  let input2 = document.querySelector("input.input5")
+
+  if(mes == "" || mes.length < 2) {
+    h6.classList.remove("hide")
+    input.classList.add("input-border")
+    return false
+    
+  } else if (ano == "" || ano.length < 2) {
+    h6.classList.remove("hide")
+    input2.classList.add("input-border")
+    return false
+    
+  } else {
+    h6.classList.add("hide")
+    input2.classList.remove("input-border")
+    input.classList.remove("input-border")
+
+    return true
+  }
+}
+
 //Adicionando o CVC do Cartão
 let inputCVC = document.querySelector(`input.cvc`)
 
 inputCVC.addEventListener(`keyup`, atualizarCVC)
+inputCVC.addEventListener(`keyup`, verificarCVC)
 
+//Função para atualizar o CVC
 function atualizarCVC() {
   let cvc = document.querySelector(`input.cvc`).value
 
@@ -79,3 +157,40 @@ function atualizarCVC() {
   }
 
 }
+
+//Verificação do CVC
+function verificarCVC() {
+  let cvc = document.querySelector(`input.cvc`).value
+
+  let h6 = document.querySelector("h6.area-4")
+  let input = document.querySelector("input.input-3")
+
+  if(cvc == "" || cvc.length < 3) {
+    h6.classList.remove("hide")
+    input.classList.add("input-border")
+    return false
+    
+  } else {
+    h6.classList.add("hide")
+    input.classList.remove("input-border")
+    return true
+  }
+}
+
+// Adicionando verificação dos valores dos inputs
+let btn = document.querySelector("div button.btn")
+
+btn.addEventListener("click", function() {
+  let nome = verificarNome()
+  let numero = verificarNumero()
+  let data = verificarData()
+  let cvc = verificarCVC()
+
+  if(nome == true && numero == true && data == true && cvc == true) {
+    window.location.href = "../html/obrigado.html"
+  } 
+  
+
+  console.log(nome, numero, data, cvc)
+})
+
